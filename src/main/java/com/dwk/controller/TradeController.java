24b6390d6bb591d47e05ddff22d9ae51018f8570
easeeit <easeeit@126.com>
2014-11-12 23:27:04 +0800

    home

--- src/main/java/com/dwk/controller/TradeController.java
+++ src/main/java/com/dwk/controller/TradeController.java
@@ -1,13 +1,16 @@
 package com.dwk.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.dwk.constant.APIConstant;
 import com.dwk.constant.DataConstant;
@@ -25,20 +28,28 @@ import com.dwk.service.trade.TradeService;
  */
 @Controller
 @RequestMapping("/trade")
+@SuppressWarnings("rawtypes")
 public class TradeController extends BaseController {
 
   @Autowired
   private TradeService tradeService;
 
-  @RequestMapping(value = "/list/{pageNum}/{rowNum}", produces = APIConstant.CONTENT_TYPE_JSON)
+  @RequestMapping(value = { "/list/{pageNum}/{rowNum}", "/list/{pageNum}", "/list" }, produces = APIConstant.CONTENT_TYPE_JSON)
   @ResponseBody
-  public String list(HttpServletRequest request, @PathVariable Integer pageNum, @PathVariable Integer rowNum) throws Exception {
+  public String list(HttpServletRequest request) throws Exception {
     try {
-      if (pageNum <= 0) {
-        pageNum = DataConstant.PN;
-      }
-      if (rowNum <= 0) {
-        rowNum = DataConstant.RN;
+      Integer pageNum = DataConstant.PN;
+      Integer rowNum = DataConstant.RN;
+      Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+      if (pathVariables != null) {
+        if (pathVariables.containsKey("pageNum")) {
+          pageNum = NumberUtils.toInt("" + pathVariables.get("pageNum"), pageNum);
+          pageNum = pageNum > 0 ? pageNum : DataConstant.PN;
+        }
+        if (pathVariables.containsKey("rowNum")) {
+          rowNum = NumberUtils.toInt("" + pathVariables.get("rowNum"), rowNum);
+          rowNum = rowNum > 0 ? rowNum : DataConstant.RN;
+        }
       }
       return outResponse(tradeService.list(pageNum, rowNum));
     } catch (ServiceException sex) {
@@ -52,7 +63,7 @@ public class TradeController extends BaseController {
 
   @RequestMapping(value = "/create", produces = APIConstant.CONTENT_TYPE_JSON)
   @ResponseBody
-  public String create(HttpServletRequest request ) throws Exception {
+  public String create(HttpServletRequest request) throws Exception {
     try {
       LoginUser user = getUser();
       return outResponse(tradeService.create(user, request.getParameterMap()));
@@ -82,7 +93,8 @@ public class TradeController extends BaseController {
 
   @RequestMapping(value = "/update_status", produces = APIConstant.CONTENT_TYPE_JSON)
   @ResponseBody
-  public String updateStatus(HttpServletRequest request, @RequestParam String id, @RequestParam String status) throws Exception {
+  public String updateStatus(HttpServletRequest request, @RequestParam String id, @RequestParam String status)
+      throws Exception {
     try {
       LoginUser user = getUser();
       return outResponse(tradeService.updateTradeStatus(user, id, status));
@@ -94,7 +106,5 @@ public class TradeController extends BaseController {
       return outResponse("trade update status", ex);
     }
   }
-
- 
 
 }
